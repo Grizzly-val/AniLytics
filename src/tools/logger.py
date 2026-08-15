c9e911977eba6e3500110d0d751f47e7a772c019
Grizzly-val <lanz.10donatos@gmail.com>
2026-08-15 20:39:58 +0800

    fixed: logs printing to terminal

--- src/tools/logger.py
+++ src/tools/logger.py
@@ -1,27 +1,38 @@
 
 import logging
 
+
 class Logger:
-    def __init__(self, log_file_path: str, log_level: str = 'INFO'):
+    def __init__(self, log_file_path: str, log_level: str = 'INFO', log_to_console: bool = False):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(getattr(logging, log_level.upper(), logging.INFO))
+        self.logger.propagate = False
+
+        for handler in list(self.logger.handlers):
+            self.logger.removeHandler(handler)
+            handler.close()
+
+        level = getattr(logging, log_level.upper(), logging.INFO)
 
         # Create file handler which logs messages to a file
-        file_handler = logging.FileHandler(log_file_path, mode= 'w')
-        file_handler.setLevel(getattr(logging, log_level.upper(), logging.INFO))
+        file_handler = logging.FileHandler(log_file_path, mode='w', encoding='utf-8')
+        file_handler.setLevel(level)
 
-        # Create console handler which logs messages to the console
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(getattr(logging, log_level.upper(), logging.INFO))
+        # Create console handler only when explicitly requested
+        console_handler = None
+        if log_to_console:
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(level)
 
         # Create formatter and add it to the handlers
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
-        console_handler.setFormatter(formatter)
 
         # Add the handlers to the logger
         self.logger.addHandler(file_handler)
-        self.logger.addHandler(console_handler)
+        if console_handler is not None:
+            console_handler.setFormatter(formatter)
+            self.logger.addHandler(console_handler)
 
     def debug(self, message: str):
         self.logger.debug(message)
